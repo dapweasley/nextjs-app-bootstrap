d8ebcc88a157b06c6eb0ac7ee2515f5261689f31
dapweasley <162858137+dapweasley@users.noreply.github.com>
2025-07-26 21:31:42 +0700

    Add next.config.js 1753540301726

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     unoptimized: true,
   },
-  // For deployment on platforms like Vercel, Netlify, etc.
   output: 'standalone',
 }
 
