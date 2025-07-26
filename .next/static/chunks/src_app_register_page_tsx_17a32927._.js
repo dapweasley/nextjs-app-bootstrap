846cb3fce3830eb6ef00dc19d3cc2b825841dbc5
dapweasley <162858137+dapweasley@users.noreply.github.com>
2025-07-26 20:10:21 +0700

    Add .next/static/chunks/src_app_register_page_tsx_17a32927._.js 1753535420449

---
+++ .next/static/chunks/src_app_register_page_tsx_17a32927._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_1878b318._.js",
+  "static/chunks/src_a800d36e._.js"
+],
+    source: "dynamic"
+});
